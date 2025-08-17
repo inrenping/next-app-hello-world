f10467c2ff18e1108fb91e5aa5224908f06b7f14
inrenping <inrenping@gmail.com>
2025-08-17 19:46:42 +0800

    update add daisyui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -50,7 +50,7 @@ interface WeatherResponse {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('http://t.weather.sojson.com/api/weather/city/101030100',
+  const res = await fetch('http://t.weather.sojson.com/api/weather/city/101030100', { cache: 'no-store' }
   )
   const weather: WeatherResponse = await res.json()
   return (
@@ -58,9 +58,18 @@ const UsersPage = async () => {
       <table className='table table-bordered'>
         <thead>
           <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
+            <th>date</th>
+            <th>high</th>
+            <th>low</th>
+            <th>ymd</th>
+            <th>week</th>
+            <th>sunrise</th>
+            <th>sunset</th>
+            <th>aqi</th>
+            <th>fx</th>
+            <th>fl</th>
+            <th>type</th>
+            <th>notice</th>
           </tr>
         </thead>
         <tbody>
@@ -69,6 +78,15 @@ const UsersPage = async () => {
               <td>{w.date}</td>
               <td>{w.high}</td>
               <td>{w.low}</td>
+              <td>{w.ymd}</td>
+              <td>{w.week}</td>
+              <td>{w.sunrise}</td>
+              <td>{w.sunset}</td>
+              <td>{w.aqi}</td>
+              <td>{w.fx}</td>
+              <td>{w.fl}</td>
+              <td>{w.type}</td>
+              <td>{w.notice}</td>
             </tr>
           ))}
         </tbody>
